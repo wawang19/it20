6f53ad2dfe089a89e22937b0e7cb059c3db6785d
wawang19 <ellevera.oscarjay19@gmail.com>
2024-09-25 17:29:48 +0800

    added simple array

---
+++ simplearray.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package simplearray;
+
+/**
+ *
+ * @author kentr
+ */
+public class SimpleArray {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] names = {"kent", "josh", "james"};
+        
+        System.out.println("Simple Names: " + names[0]);
+        System.out.println("Simple Names: " + names[1]);
+        System.out.println("Simple Names: " + names[2]);
+      
+
+    }
+
+}
