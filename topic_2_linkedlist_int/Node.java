05fc8ab419a1a466033c5628cad7240e85bd603b
wawang19 <ellevera.oscarjay19@gmail.com>
2024-10-04 01:20:58 +0800

    added integer linkedlist

---
+++ topic_2_linkedlist_int/Node.java
@@ -0,0 +1,16 @@
+
+
+
+package topic_2_linkedlist_int;
+
+
+public class Node {
+    int data;  
+    Node next;  
+
+    // Constructor
+    Node(int data) {
+        this.data = data;
+        this.next = null;
+}
+}
